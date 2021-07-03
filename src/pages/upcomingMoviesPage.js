aced69b2aeef6767cb031de8aeb4bfd1c5e9c1a8
Seamus McCarthy <47305550+spoderman01@users.noreply.github.com>
2021-07-03 12:36:21 +0100

    Change upcoming movies page to use caching.

--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -1,27 +1,40 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { getUpcomingMovies } from "../api/tmdb-api";
-import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+// import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'git
 import AddToPlaylistIcon from '../components/cardIcons/addToPlaylist'
+import { useQuery } from 'react-query'
+import Spinner from '../components/spinner'
 
 const UpcomingPage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
+  // const [movies, setMovies] = useState([]);
+  // const favorites = movies.filter(m => m.favorite)
+  // localStorage.setItem('favorites', JSON.stringify(favorites))
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  // const addToFavorites = (movieId) => {
+  //   const updatedMovies = movies.map((m) =>
+  //     m.id === movieId ? { ...m, favorite: true } : m
+  //   );
+  //   setMovies(updatedMovies);
+  // };
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  // useEffect(() => {
+  //   getUpcomingMovies().then(movies => {
+  //     setMovies(movies);
+  //   });
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
+
+  const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }  
+  const movies = data.results;
 
   return (
     <PageTemplate
