fabe84a88df0a1444c87f3275e4236420d5bfe31
Seamus McCarthy <47305550+spoderman01@users.noreply.github.com>
2021-07-08 20:20:47 +0100

    Remove compilation warnings and general tidy up

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -10,7 +10,6 @@ import Typography from "@material-ui/core/Typography";
 import FavoriteIcon from "@material-ui/icons/Favorite";
 import CalendarIcon from "@material-ui/icons/CalendarTodayTwoTone";
 import StarRateIcon from "@material-ui/icons/StarRate";
-// import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
 import img from '../../images/film-poster-placeholder.png'
 import { Link } from "react-router-dom";
@@ -27,7 +26,7 @@ const useStyles = makeStyles({
 
 export default function MovieCard({ movie, action }) {
   const classes = useStyles();
-  const { favorites, addToFavorites } = useContext(MoviesContext);
+  const { favorites } = useContext(MoviesContext);
 
   if (favorites.find((id) => id === movie.id)) {
     movie.favorite = true;
@@ -35,14 +34,8 @@ export default function MovieCard({ movie, action }) {
     movie.favorite = false
   }
 
-  const handleAddToFavorite = (e) => {
-    e.preventDefault();
-    addToFavorites(movie);
-  };
-
   return (
     <Card className={classes.card}>
-      {/* <CardHeader className={classes.header} title={movie.title} /> */}
       <CardHeader
       className={classes.header}
       avatar={
@@ -83,9 +76,6 @@ export default function MovieCard({ movie, action }) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-      {/* <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-      </IconButton> */}
         {action(movie)}
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
--- src/pages/upcomingMoviesPage.js
+++ src/pages/upcomingMoviesPage.js
@@ -7,23 +7,6 @@ import { useQuery } from 'react-query'
 import Spinner from '../components/spinner'
 
 const UpcomingPage = (props) => {
-  // const [movies, setMovies] = useState([]);
-  // const favorites = movies.filter(m => m.favorite)
-  // localStorage.setItem('favorites', JSON.stringify(favorites))
-
-  // const addToFavorites = (movieId) => {
-  //   const updatedMovies = movies.map((m) =>
-  //     m.id === movieId ? { ...m, favorite: true } : m
-  //   );
-  //   setMovies(updatedMovies);
-  // };
-
-  // useEffect(() => {
-  //   getUpcomingMovies().then(movies => {
-  //     setMovies(movies);
-  //   });
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
 
   const {  data, error, isLoading, isError }  = useQuery('upcoming', getUpcomingMovies)
 
